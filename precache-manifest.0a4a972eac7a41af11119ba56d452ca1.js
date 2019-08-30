73651b349a6b508711aaf77af1691fcb6d665ee4
nishchay-v <46082042+nishchay-v@users.noreply.github.com>
2019-08-31 00:59:34 +0530

    Updates

---
+++ precache-manifest.0a4a972eac7a41af11119ba56d452ca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b564feb150fb08f201aaf53fbb534b34",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a4c405cc663d541fad1d",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "1926cda277f9441b8237",
+    "url": "/robofriends/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a4c405cc663d541fad1d",
+    "url": "/robofriends/static/js/2.52355532.chunk.js"
+  },
+  {
+    "revision": "1926cda277f9441b8237",
+    "url": "/robofriends/static/js/main.ae9642dc.chunk.js"
+  },
+  {
+    "revision": "8bd552311f5d528a5605",
+    "url": "/robofriends/static/js/runtime~main.e3d5a5e7.js"
+  }
+]);
